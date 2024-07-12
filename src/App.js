817572adbf1823aace0e96a41f951dd1a5e3b41f
Yash Verma <vermayash88991@gmail.com>
2024-07-12 14:32:02 +0530

    Added specials and footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 import Main from './components/Main';
 import Menu from './components/Menu';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Nav/>
      <Main/>
      <Menu/>
+     <Footer/>
     </>
     </BrowserRouter>
   );
