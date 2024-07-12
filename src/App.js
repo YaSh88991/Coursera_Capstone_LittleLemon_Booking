94ecab33b476f8982b9cd8fc0077b65022580979
Yash Verma <vermayash88991@gmail.com>
2024-07-12 17:55:49 +0530

    Added testimonials, completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from './components/Main';
 import Menu from './components/Menu';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
      <Nav/>
      <Main/>
      <Menu/>
+     <Testimonials/>
      <Footer/>
     </>
     </BrowserRouter>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import Testimonials from './components/Testimonials';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App component with all child components', () => {
+  const { getByTestId } = render(<App />);
+  
+  // Check if each component is rendered
+  expect(getByTestId('nav-component')).toBeInTheDocument();
+  expect(getByTestId('main-component')).toBeInTheDocument();
+  expect(getByTestId('menu-component')).toBeInTheDocument();
+  expect(getByTestId('testimonials-component')).toBeInTheDocument();
+  expect(getByTestId('footer-component')).toBeInTheDocument();
+
+})
+
+test('renders Menu component', () => {
+  render(<Menu/>);
 });
+
+test('renders Footer component', () => {
+  render(<Footer/>);
+});
+
+test('renders Testimonials component', () => {
+  render(<Testimonials/>);
+});
+
+test('renders Testimonials header', () => {
+  const { getByText } = render(<Testimonials />);
+  const headerElement = getByText('Testimonials');
+  expect(headerElement).toBeInTheDocument();
+});
+
